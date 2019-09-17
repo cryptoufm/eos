ab22f7e18d2b14be232754e6a1050ec71d537f9f
Isabel Melendez <isa.melendez@ubiquo.io>
2019-09-17 21:01:06 +0000

    menu working - dummy

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,6 +3,12 @@ import PropTypes              from 'prop-types'
 import { withRouter }       from 'react-router-dom'
 import { Paper }              from '@material-ui/core'
 import { styles }             from './styles.css'
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
 
 class Footer extends Component {
   render() {
@@ -12,7 +18,13 @@ class Footer extends Component {
       <div className={styles}>
         <Paper>
           <div className={`bottom-navigation`}>
-            Hola
+          <BottomNavigation
+              showLabels
+              >
+              <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+              <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+              <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+            </BottomNavigation>
           </div>
         </Paper>
       </div>
