c02c4bb820e8c2319ad3c6592722fce954d18036
Carlos Cujcuj <eduardoc203@gmail.com>
2019-09-17 15:18:46 -0600

    ui profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Footer from './Components/Footer'
+import Perfil from './Components/Perfil'
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          Learn Reactt
-        </div>
-        {/* ASI LLAMAN A SU COMPONENTE PARA MIENTRAS LLAMENLO ACA Y CREEN SU CARPETA ADENTRO DE COMPONENTS
-          <Footer />
-        */}
-      </header>
+        <Perfil />
     </div>
   );
 }
+
 
 export default App;
