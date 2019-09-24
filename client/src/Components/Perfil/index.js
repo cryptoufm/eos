8df5d6345c602c824884d2104b0d7f3f6e597c77
Carlos Cujcuj <eduardoc203@gmail.com>
2019-09-23 19:25:56 -0600

    cujperfil

--- client/src/Components/Perfil/index.js
+++ client/src/Components/Perfil/index.js
@@ -10,18 +10,20 @@ import AccountTreeIcon from '@material-ui/icons/AccountTree';
 import Footer from '../Footer';
 import firebase from "firebase"
 import EmailIcon from '@material-ui/icons/Email';
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+
 
 // import { Api, JsonRpc, RpcError } from 'eosjs';
 // import { Api, JsonRpc, RpcError } from 'eosjs';
 // import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';
-
+var nombres;
 
 class Profile extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      list: []
+      info: []
     }
   }
 
@@ -29,24 +31,29 @@ class Profile extends Component {
     firebase.auth().onAuthStateChanged(user => {
       this.setState({ isSignedIn: !!user })
         console.log("user", user)
+
+      nombres = firebase.auth().currentUser.displayName;
+      fetch(`/api/getProfile?uid=${firebase.auth.currentUser.uid}`)
+      .then(res => res.json())
+      .then(info => this.setState({ info }))
+      .catch(err => err)
+
+      //let rowsjson = JSON.parse("[" + this.state.info + "]");
+      //const json = rowsjson[0]
+
     })
 
-    //this.getList();
-    //console.log(this.state.list)
+    // function llamar_carlos():
+    //    cujcuj = cujcujx3
+    //    return(carlos aparece)
   }
 
-  // getList = () => {
-  //   fetch('54.163.3.27:5000/createAccount?uid=${uid}&account=${user}')
-  //   .then(res => res.json())
-  //   .then(list => this.setState({ list }))
-  //   .catch(err => err)
-  // }
-
+  
   render() {
+
 
     var user = firebase.auth().currentUser;
     var name, email, photoUrl, uid, emailVerified, name2;
-    console.log(user);
 
     if (user != null) {
       name = user.displayName;
@@ -55,37 +62,37 @@ class Profile extends Component {
       uid = user.uid;
       name2 = name.replace(/ +/g, "");
     }
-    console.log(uid);
 
-    //var resp;
 
-    fetch(`http://54.163.3.27:5000/createAccount?uid=${uid}&account=${name2}`)
+    /*
+    fetch(`/api/getBalance?uid=${uid}`)
       .then(res => res.json())
-      .then(data => resp = data)
-      .then(() => console.log(resp))
+      .then(balance => this.setState({ balance }))
       .catch(err => err)
+    */
     //const urls = `http://54.163.3.27:5000/createAccount?uid=${uid}&account=${name2}`
 
 
     const { list } = this.state;
     console.log("lista", list) 
-    //resp = JSON.parse(resp);
-    //http://54.163.3.27:5000/createAccount?uid=askdfjqkwjerr&account=ricardojmv
+
+     
 
     return (
 
     <div className="page">
         <Footer />
         <div className="header">
-            <img className="user-photo" src={photoUrl} />
+            
         </div>
 
         <div className="detalles">
+        <img className="user-photo" src={photoUrl} />
         <div className="hola">
         <Grid container alignItems="center">
           <Grid item xs>
             <Typography gutterBottom variant="h6">
-              {obj}
+              
             </Typography>
             <Typography gutterBottom variant="h4" >
               {name} 
@@ -100,7 +107,7 @@ class Profile extends Component {
         {/*CUENTA */}
         <Grid container alignItems="center" className="cuenta">
             <Grid item xs={1}>
-            <EmailIcon color="secondary" />
+            <PersonIcon color="secondary" />
             </Grid>
             <Grid item>
                 <Typography gutterBottom variant="h7">
@@ -117,12 +124,12 @@ class Profile extends Component {
             </Grid>
             <Grid item>
                 <Typography gutterBottom variant="h6">
-                    150.0000 Mises
+                20000
                 </Typography>
             </Grid>
         </Grid>
 
-        <Button variant="outlined" color="primary"> Private Key</Button>
+        <Button variant="outlined" color="primary" onClick={() => alert("5Js36uNKqHvanD6MU2XScMHchs5k733azdRRKzGJcQT3roTmdCd")} > Private Key</Button>
         <p> </p>
         <Button variant="outlined" color="primary" onClick={() => firebase.auth().signOut()}>Sign out</Button>
       </div>
@@ -135,88 +142,3 @@ class Profile extends Component {
 
 
   export default Profile
-
-
-// function Profile() {
-//     // const defaultPrivateKey = "5JZ2YfAQewJxPEspQ7aqcdZx5eNPkPQgpRw1WopwixLU56XAeL8"; // bob
-//     // const signatureProvider = new JsSignatureProvider([defaultPrivateKey]);
-//     // const rpc = new JsonRpc('https://jungle2.cryptolions.io', { fetch });
-//     // rpc.get_currency_balance('eosio.token', 'ricardojmv54', 'EOS').then((balance) => console.log("account", balance));
-//     return (
-//     <div className="page">
-//         <div className="header">
-//             {/*
-//             <img 
-//                 className="user-photo"
-//                 //src="https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/19990550_320504011710462_6233984476058453731_n.jpg?_nc_cat=108&_nc_oc=AQmR0T4NP73IDG4np1E9mU5bJNsAmGzlCxrtRzb5ol8ztra1rcefx1Wx0p3lHMhlbag&_nc_ht=scontent-mia3-2.xx&oh=ff41751e6aca773d7510a135c8e4f800&oe=5DF13CFA"
-//                 src={Usuario}
-//             />
-//             */}
-//         </div>
-//         <div className="detalles">
-            
-
-//         <div className="hola">
-//         <Grid container alignItems="center">
-//           <Grid item xs>
-//             <Typography gutterBottom variant="h4">
-//             Satoshi
-//             </Typography>
-//             <Typography gutterBottom variant="h4">
-//             Nakamoto
-//             </Typography>
-//           </Grid>
-//           <Grid item>
-//             <Typography gutterBottom variant="h6">
-            
-//             </Typography>
-//           </Grid>
-//         </Grid>
-
-//         {/*CUENTA */}
-//         <Grid container alignItems="center" className="cuenta">
-//             <Grid item xs={1}>
-//             <PersonIcon color="secondary" />
-//             </Grid>
-//             <Grid item>
-//                 <Typography gutterBottom variant="h6">
-//                     NakaToshi2323
-//                 </Typography>
-//             </Grid>
-//         </Grid>
-
-//         {/*Balance */}
-//         <Grid container alignItems="center" className="cuenta">
-//             <Grid item xs={1}>
-//             <LocalAtmIcon color="secondary" />
-//             </Grid>
-//             <Grid item>
-//                 <Typography gutterBottom variant="h6">
-//                     150.0000 MarroCoins
-//                 </Typography>
-//             </Grid>
-//         </Grid>
-
-
-//         {/*Cuenta Publica
-//         <Grid container alignItems="center" className="cuenta">
-//             <Grid item xs={1}>
-//             <AccountTreeIcon color="secondary" />
-//             </Grid>
-//             <Grid item>
-//                 <Typography gutterBottom variant="h6">
-//                     150.0000 MarroCoins
-//                 </Typography>
-//             </Grid>
-//         </Grid>
-//         */}
-
-//         <Button variant="outlined" color="primary"> Private Key</Button>
-//       </div>
-
-//         </div>
-//     </div>
-//     );
-//   }
-
-//   export default Profile;
--- client/src/Login/index.js
+++ client/src/Login/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import firebase from "firebase"
 import './style.css';
 import Grid from '@material-ui/core/Grid';
+import Tesoro from './treasure.png'
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
 firebase.initializeApp({
@@ -10,7 +11,12 @@ firebase.initializeApp({
 })
 
 class Login extends Component {
-
+  constructor(props){
+    super(props);
+    this.state = {
+      respuesta: []
+    }
+  }
 
   state = { isSignedIn: false }
   uiConfig = {
@@ -30,24 +36,59 @@ class Login extends Component {
     firebase.auth().onAuthStateChanged(user => {
       this.setState({ isSignedIn: !!user })
       console.log("user", user)
+
+      var nombre = firebase.auth.currentUser.displayName;
+      var name2 = nombre.replace(/ +/g, "");
+      /*
+      fetch(`/api/createAccount?uid=${firebase.auth.currentUser.uid}&username=${name2}`)
+      .then(res => res.json())
+      .then(respuesta => this.setState({ respuesta }))
+      .catch(err => err)
+      */
+
+
     })
+
+    
   }
+
   render() {
+
+    var user = firebase.auth().currentUser;
+    var name, email, photoUrl, uid, emailVerified, name2;
+
+    if (user != null) {
+      name = user.displayName;
+      email = user.email;
+      photoUrl = user.photoURL;
+      uid = user.uid;
+      name2 = name.replace(/ +/g, "");
+    }
+
+    
+
+
     return (
-      <div>
-        <div>Hola</div>
+      <div className="pageI">
         {this.state.isSignedIn ? (
-          <div>
+          <div className="d1">
+            <div>
+            <img className="photo-tes"
+                alt="profile picture"
+                src={Tesoro}
+              />
+            </div>
+            <h1>Bienvenido</h1>
+            <h3>{firebase.auth().currentUser.displayName}</h3>
             <div>
               <img className="photo-user"
                 alt="profile picture"
                 src={firebase.auth().currentUser.photoURL}
               />
             </div>
-            <div>Signed In!</div>
             <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
-            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
-            
+      
+        
           </div>
         ) : (
           <StyledFirebaseAuth
